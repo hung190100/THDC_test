26b41f1c57ab463ad1fd22f2c96fe9635a0672a5
hung190100 <45819903+hung190100@users.noreply.github.com>
2022-01-11 22:12:01 +0700

    Update B2.c

--- B2.c
+++ B2.c
@@ -10,31 +10,26 @@ int main()
         scanf("%d", &n);
     } while (n < 2 || n > 10);
 
+    int a[n];
 
-
-    if (n == 1) {
-        printf("0");
-    }  else {
-        int a[n];
-        for (int i = 0; i < n; i++) {
-            scanf("%d", &a[i]);
-        }
-
-        int b[n-1];
-
-        for (int i = 0; i < n - 1; i++) {
-            b[i] = a[i] * a[i+1];
-        }
-
-        int max = b[0];
-        for (int i = 1; i < n - 1; i++) {
-            if (b[i] > max) {
-                max = b[i];
-            }
-        }
-
-        printf("%d", max);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
     }
+
+    int b[n-1];
+
+    for (int i = 0; i < n - 1; i++) {
+        b[i] = a[i] * a[i+1];
+    }
+
+    int max = b[0];
+    for (int i = 1; i < n - 1; i++) {
+        if (b[i] > max) {
+            max = b[i];
+        }
+    }
+
+    printf("%d", max);
 
     return 0;
 }
