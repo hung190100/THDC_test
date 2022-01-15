5c1ae212dad8f40c3d94c9bd1d76cde9ab3aa427
hung190100 <45819903+hung190100@users.noreply.github.com>
2022-01-15 09:51:01 +0700

    Update B3.c

--- B3.c
+++ B3.c
@@ -6,7 +6,7 @@ int check_SNT(int n) {
     if (n == 2 || n == 3) {
         return 1;
     }
-    for (int i = 2; i < n/2 + 1; i++) {
+    for (int i = 2; i <= n/2; i++) {
         if (n%i == 0) {
             return 0;
         }
