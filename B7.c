4bff24db1c308d9f36bd3d0668270237186d9789
hung190100 <45819903+hung190100@users.noreply.github.com>
2022-01-16 18:43:24 +0700

    Update B7.c

--- B7.c
+++ B7.c
@@ -9,6 +9,10 @@ int main()
     if (c1 < 65 || c1 > 122 || (c1 > 90 && c1 < 97) || c2 < 65 || c2 > 122 || (c2 > 90 && c2 < 97)) {
         printf ("Error");
     } else {
+        if (c1 == c2 || c1 == c2 + 1 || c2 == c1 + 1) {
+            printf("Null");
+        }
+        
         if (c1 > c2 && c1 >= 97 && c2 <= 90) {
             while (c2 != 90) {
                 printf("%c ", ++c2);
@@ -26,11 +30,11 @@ int main()
                 printf("%c ", c++);
             }
         } else if (c1 > c2) {
-            for (int i = c2+1; i < c1; i++) {
+            for (int i = c2 + 1; i < c1; i++) {
                 printf("%c ", i);
             }
         } else {
-            for (int i = c1+1; i < c2; i++) {
+            for (int i = c1 + 1; i < c2; i++) {
                 printf("%c ", i);
             }
         }
