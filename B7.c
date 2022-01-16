db7407d009a4c014b8408bbb58555f0e4a9f5266
hung190100 <45819903+hung190100@users.noreply.github.com>
2022-01-16 18:40:42 +0700

    Create B7.c

---
+++ B7.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c1, c2;
+    scanf("%c %c", &c1, &c2);
+
+    if (c1 < 65 || c1 > 122 || (c1 > 90 && c1 < 97) || c2 < 65 || c2 > 122 || (c2 > 90 && c2 < 97)) {
+        printf ("Error");
+    } else {
+        if (c1 > c2 && c1 >= 97 && c2 <= 90) {
+            while (c2 != 90) {
+                printf("%c ", ++c2);
+            }
+            char c = 'a';
+            while (c != c1) {
+                printf("%c ", c++);
+            }
+        } else if (c1 < c2 && c2 >= 97 && c1 <= 90) {
+            while (c1 != 90) {
+                printf("%c ", ++c1);
+            }
+            char c = 'a';
+            while (c != c2) {
+                printf("%c ", c++);
+            }
+        } else if (c1 > c2) {
+            for (int i = c2+1; i < c1; i++) {
+                printf("%c ", i);
+            }
+        } else {
+            for (int i = c1+1; i < c2; i++) {
+                printf("%c ", i);
+            }
+        }
+    }
+
+    return 0;
+}
